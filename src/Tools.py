c310f3f72f1a1f5983479e22d4d6ba6071225717
Edcwsyh <const.nullPointer@gmail.com>
2022-10-24 10:15:19 +0800

    [n][新增变量用于控制输出内容是否有不同颜色]

--- src/Tools.py
+++ src/Tools.py
@@ -9,15 +9,25 @@ class Color :
     YELLOW = 3
     BLUE = 4
 
+class PrintColorState : 
+    ENABLE = 1      # 开启
+    DISABLE = 2     # 关闭
+
+# 是否开启颜色打印, 若输出终端异常, 请关闭该选项
+envPrintColorState = PrintColorState.ENABLE
+
 def get_color_txt( color ) : 
     switcher = {
         Color.RED : "\033[31m", 
-        Color.GREEN : "\033[32m", 
+        Color.GREEN : "\033[32m",       
         Color.YELLOW : "\033[33m", 
         Color.BLUE : "\033[34m", 
     }
     return switcher.get( color )
 
 def print_color(string, color) : 
-    newString = get_color_txt( color ) + string + "\033[0m"
-    print( newString )
+    if envPrintColorState == PrintColorState.ENABLE : 
+        newString = get_color_txt( color ) + string + "\033[0m"
+        print( newString )
+    else : 
+        print( string )
